509797062389c97f506d04909055bd8c8949b3c8
Diego Reis <diegoreso@me.com>
2025-06-26 15:44:59 -0300

    feat: creates endpoint to get a user by id

--- src/main/java/dev/reso/workshop/springbootmongo/resource/UserResource.java
+++ src/main/java/dev/reso/workshop/springbootmongo/resource/UserResource.java
@@ -5,6 +5,7 @@ import dev.reso.workshop.springbootmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class UserResource {
     public ResponseEntity<List<UserDTO>>  getUsers() {
         List<UserDTO> usersDTO = service.findAll().stream().map(UserDTO::new).toList();
         return ResponseEntity.ok(usersDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        UserDTO userDTO = new UserDTO(service.findById(id));
+        return ResponseEntity.ok(userDTO);
     }
 
 }
