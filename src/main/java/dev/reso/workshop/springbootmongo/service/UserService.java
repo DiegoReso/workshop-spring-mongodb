3177f2c3fb186dde5ca6b0d196c59d4f022bcb1c
Diego Reis <diegoreso@me.com>
2025-06-26 15:46:14 -0300

    feat: creates a method to return a user by id, if not found, throw a exception

--- src/main/java/dev/reso/workshop/springbootmongo/service/UserService.java
+++ src/main/java/dev/reso/workshop/springbootmongo/service/UserService.java
@@ -2,6 +2,7 @@ package dev.reso.workshop.springbootmongo.service;
 
 import dev.reso.workshop.springbootmongo.domain.User;
 import dev.reso.workshop.springbootmongo.repository.UserRepository;
+import dev.reso.workshop.springbootmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +17,9 @@ public class UserService {
     public List<User> findAll(){
         return repository.findAll();
     }
+
+    public User findById(String id){
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
 }
