961ddcde2d0e491ef59d214eeb96c4a3a2907756
Diego Reis <diegoreso@me.com>
2025-06-26 14:55:41 -0300

    feat: changes id type and adds annotation for document and id

--- src/main/java/dev/reso/workshop/springbootmongo/domain/User.java
+++ src/main/java/dev/reso/workshop/springbootmongo/domain/User.java
@@ -3,13 +3,17 @@ package dev.reso.workshop.springbootmongo.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Document(collection = "user")
 public class User {
 
-    private Long id;
+    @Id
+    private String id;
     private String name;
     private String email;
 
