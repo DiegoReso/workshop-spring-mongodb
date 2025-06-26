501954ea7f4773c7821730f2e8f4d085fbc9c031
Diego Reis <diegoreso@me.com>
2025-06-26 13:38:30 -0300

    project created

---
+++ src/test/java/dev/reso/workshop/springbootmongo/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.reso.workshop.springbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
