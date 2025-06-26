d9b4d90b6d1f20ae518573076999e9aacb2cccfb
Diego Reis <diegoreso@me.com>
2025-06-26 14:26:39 -0300

    feat: creates entity user

---
+++ src/main/java/dev/reso/workshop/springbootmongo/domain/User.java
@@ -0,0 +1,16 @@
+package dev.reso.workshop.springbootmongo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+
+}
