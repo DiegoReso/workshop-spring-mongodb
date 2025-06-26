c5bc8997beda43de2b0f314514d9b67da449c9ef
Diego Reis <diegoreso@me.com>
2025-06-26 16:06:40 -0300

    feat: creates a endpoint to insert a User

--- src/main/java/dev/reso/workshop/springbootmongo/resource/UserResource.java
+++ src/main/java/dev/reso/workshop/springbootmongo/resource/UserResource.java
@@ -1,13 +1,12 @@
 package dev.reso.workshop.springbootmongo.resource;
 
+import dev.reso.workshop.springbootmongo.domain.User;
 import dev.reso.workshop.springbootmongo.dto.UserDTO;
 import dev.reso.workshop.springbootmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +27,14 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id){
         UserDTO userDTO = new UserDTO(service.findById(id));
         return ResponseEntity.ok(userDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO objDto){
+        User user = service.fromDTO(objDto);
+        user = service.insert(user);
+        objDto.setId(user.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(objDto);
     }
 
 }
