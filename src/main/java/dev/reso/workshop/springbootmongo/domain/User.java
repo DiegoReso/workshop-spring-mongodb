770a0a1922c5cd01fd62dfd9f92bc73bc99bc0bc
Diego Reis <diegoreso@me.com>
2025-06-30 19:49:59 -0300

    feat: Create a contructor receiving a UserDTO and add reference for Post

--- src/main/java/dev/reso/workshop/springbootmongo/domain/User.java
+++ src/main/java/dev/reso/workshop/springbootmongo/domain/User.java
@@ -1,10 +1,15 @@
 package dev.reso.workshop.springbootmongo.domain;
 
+import dev.reso.workshop.springbootmongo.dto.UserDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,4 +22,12 @@ public class User {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(UserDTO author) {
+        id = author.getId();
+        name = author.getName();
+        email = author.getEmail();
+    }
 }
