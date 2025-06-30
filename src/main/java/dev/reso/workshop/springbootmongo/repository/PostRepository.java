9e51c52076fb66b13f606ddc906240b1b408d8ef
Diego Reis <diegoreso@me.com>
2025-06-30 17:33:15 -0300

    feat: Implement PostRepository interface

---
+++ src/main/java/dev/reso/workshop/springbootmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package dev.reso.workshop.springbootmongo.repository;
+
+import dev.reso.workshop.springbootmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
