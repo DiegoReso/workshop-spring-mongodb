d59edf22424d6cb41766972276ba94476612c395
Diego Reis <diegoreso@me.com>
2025-06-30 19:50:53 -0300

    feat: Create a contructor receiving a User and add postsDTO list.

--- src/main/java/dev/reso/workshop/springbootmongo/dto/UserDTO.java
+++ src/main/java/dev/reso/workshop/springbootmongo/dto/UserDTO.java
@@ -1,8 +1,12 @@
 package dev.reso.workshop.springbootmongo.dto;
 
+import dev.reso.workshop.springbootmongo.domain.Post;
 import dev.reso.workshop.springbootmongo.domain.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Data
@@ -11,10 +15,14 @@ public class UserDTO {
     private String id;
     private String name;
     private String email;
+    private List<PostDTO> posts = new ArrayList<>();
 
     public UserDTO(User user){
         id = user.getId();
         name = user.getName();
         email = user.getEmail();
+        for(Post post : user.getPosts()){
+            posts.add(new PostDTO(post));
+        }
     }
 }
