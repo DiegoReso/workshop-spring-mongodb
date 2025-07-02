cc6aa85c3ef1522370c641ee52a4e04ef195f6bf
Diego Reis <diegoreso@me.com>
2025-07-02 15:54:36 -0300

    refact: remove list of postsDTO

--- src/main/java/dev/reso/workshop/springbootmongo/dto/UserDTO.java
+++ src/main/java/dev/reso/workshop/springbootmongo/dto/UserDTO.java
@@ -15,14 +15,11 @@ public class UserDTO {
     private String id;
     private String name;
     private String email;
-    private List<PostDTO> posts = new ArrayList<>();
+
 
     public UserDTO(User user){
         id = user.getId();
         name = user.getName();
         email = user.getEmail();
-        for(Post post : user.getPosts()){
-            posts.add(new PostDTO(post));
-        }
     }
 }
