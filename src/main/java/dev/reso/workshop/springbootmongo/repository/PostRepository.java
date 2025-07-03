bf80009c4e66d4ebd7836c76bc6695a5f66dda3a
Diego Reis <diegoreso@me.com>
2025-07-02 21:02:18 -0300

    feat: create query to return a list of posts by author

--- src/main/java/dev/reso/workshop/springbootmongo/repository/PostRepository.java
+++ src/main/java/dev/reso/workshop/springbootmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package dev.reso.workshop.springbootmongo.repository;
 
 import dev.reso.workshop.springbootmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByComments_TextContainingIgnoreCase(String comment);
 
+    @Query("{ 'author.name' : { $regex :  ?0, $options: 'i'} }")
+    List<Post> findByAuthor(String author);
 
 }
